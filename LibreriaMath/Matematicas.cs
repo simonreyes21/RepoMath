a2d63436a2503449b0c9e0617ad02e02ea773431
simonreyes21 <simreymad@alu.edu.gva.es>
2025-11-17 07:45:12 +0100

    Funcion MCD

--- LibreriaMath/Matematicas.cs
+++ LibreriaMath/Matematicas.cs
@@ -1,8 +1,20 @@
 ﻿public class Matematicas
 {
- public static int Fibonacci(int n)
- {
- if (n <= 1) return n;
- return Fibonacci(n - 1) + Fibonacci(n - 2);
- }
+    public static int Fibonacci(int n)
+    {
+        if (n <= 1) return n;
+        return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
+
+    public static int MaximoComunDivisor(int a, int b)
+    {
+        while (b != 0)
+        {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
 }
+
